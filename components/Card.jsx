0e80cc0eec840073cd70e12002bc6c5bb087f4ed
Android Studio <devandroid@ort>
2022-08-11 12:04:15 -0300

    galo diseño card

--- components/Card.jsx
+++ components/Card.jsx
@@ -10,10 +10,10 @@ const Carta = (props) => {
          
         <Card style={Styles.container}>
         <Card.Content>
-            <Title>{title}</Title>
+            <Title style={Styles.title}>{title}</Title>
         </Card.Content>
        <Card.Content>
-        <Paragraph>{text}</Paragraph>
+        <Paragraph style={Styles.paragraph}>{text}</Paragraph>
         </Card.Content>
       </Card>
          
@@ -24,7 +24,30 @@ export default Carta
 const Styles = StyleSheet.create({
     container :{
         alignContent:'center',
-        margin:37
-    }
-})
+        margin:37,
+        flex: 1,
+        justifyContent: 'center',
+        padding: 20,
+
+  },
+  paragraph: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 20
+  },
+  title:{
+    backgroundColor: `#008b8b`,
+        borderWidth:2,
+        borderColor: 'lightblue',
+        borderRadius: 15,
+        padding: 15,
+        width: '50%',
+        left: '25%',
+        justifyContent:'center',
+        marginTop:45,
+        textAlign: 'center',
+  }
+});
+
 
